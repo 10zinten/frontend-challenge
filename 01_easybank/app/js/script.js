b7790f74319693d3dd110ec88cbb49b25be80aa1
tenzin <ten13zin@gmail.com>
2020-12-10 19:38:03 +0530

    fix(easybank): animate the mobile menu

--- 01_easybank/app/js/script.js
+++ 01_easybank/app/js/script.js
@@ -1,15 +1,23 @@
 const btnHambuger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHambuger.addEventListener('click', function() {
     if (header.classList.contains('open')) { // Close Hamburger Menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function (fadeElem) {
+            fadeElem.classList.remove('fade-in');
+            fadeElem.classList.add('fade-out');
+        });
     } else { // Open Hamburger Menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function (fadeElem) {
+            fadeElem.classList.remove('fade-out');
+            fadeElem.classList.add('fade-in');
+        });
     }
 })
